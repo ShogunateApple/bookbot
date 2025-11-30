3ca6ee74c8b9acfcf942e3fa1b24b1d6b88d300a
ShogunApple <coolwillxawe@gmail.com>
2025-11-30 12:25:37 -0800

    main prints out all of frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+#returns a file as a single string
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_string = f.read()
+        return file_string
+
+#main
+def main():
+    book_string = get_book_text("books/frankenstein.txt")
+    print(book_string)
+
+
+main()
