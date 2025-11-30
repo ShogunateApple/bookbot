744edf17c1a1f78fd2dabeeb9e4f45d42a4b64a3
ShogunApple <coolwillxawe@gmail.com>
2025-11-30 12:37:20 -0800

    made main.py word count frankenstein

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ def get_book_text(file_path):
         file_string = f.read()
         return file_string
 
+#counts the number of words in the given string
+def word_count(book):
+    word_list = book.split()
+    count = len(word_list)
+    return count
+
 #main
 def main():
     book_string = get_book_text("books/frankenstein.txt")
-    print(book_string)
+    number_of_words = word_count(book_string)
+    print(f"Found {number_of_words} total words")
 
 
 main()
